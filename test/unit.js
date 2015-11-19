945a90de0e3cdbc85a1b87a46844152967c80f6f
Ryan Ramage <ryan.ramage@gmail.com>
2015-11-19 08:56:27 -0700

    more efficient to store in memory. Also _all_docs etag is what we check for signature change

--- test/unit.js
+++ test/unit.js
@@ -1,16 +1,28 @@
 var view_cache_stream = require('../lib/index')
 var test = require('tape')
-var devnull = require('dev-null');
 var async = require('async')
+var couchr = require('couchr')
+var concat = require('concat-stream')
+var db = 'http://localhost:5984/idx-edm-v5'
+var view = '/_design/idx/_view/by_id'
+var docid = 'E3331885'
 
 test('fill in this', function (t) {
-  var cache = view_cache_stream('http://localhost:5984/idx-edm-v5/_design/idx/_view/by_id')
+  var cache = view_cache_stream(db + view)
 
-  async.timesSeries(20, function(n, next) {
+  async.timesSeries(10, function(n, next) {
     run(cache, n+'', next)
-
-  }, t.end)
-
+  }, function (err) {
+    t.error(err)
+    alter(db, docid, function(err) {
+      t.error(err)
+      setTimeout(function(){
+        async.timesSeries(10, function(n, next) {
+          run(cache, n+'', next)
+        }, t.end)
+      }, 5000);
+    })
+  })
 })
 
 
@@ -21,6 +33,16 @@ function run (cache, name, cb) {
       console.timeEnd(name)
       cb()
     })
-    .pipe(devnull())
+    .pipe(concat(function(data){
+      console.log('data')
+    }))
+
+  })
+}
+
+function alter (db, docid, cb) {
+  couchr.get(db + '/' + docid, function (err, doc) {
+    doc['Legal Plan'] += 'A'
+    couchr.put(db + '/' + docid, doc, cb)
   })
 }
