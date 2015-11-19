16d55d1461e9fdc2da48066cdd4fcb64fb76c7af
Ryan Ramage <ryan.ramage@gmail.com>
2015-11-18 19:27:43 -0700

    wip

--- test/unit.js
+++ test/unit.js
@@ -1,22 +1,26 @@
-var mod = require('../lib/index');
-var ndjson = require('ndjson');
-
-var test = require('tape');
-
+var view_cache_stream = require('../lib/index')
+var test = require('tape')
+var devnull = require('dev-null');
+var async = require('async')
 
 test('fill in this', function (t) {
-  var s = mod('http://localhost:5984/idx-edm-v5/_design/idx/_view/by_id');
-  var stream = s();
-  stream.on('end', function(){
-    console.log('round 2')
-    var stream2 = s();
-    stream.pipe(ndjson.serialize()).pipe(process.stdout)
+  var cache = view_cache_stream('http://localhost:5984/idx-edm-v5/_design/idx/_view/by_id')
+
+  async.timesSeries(20, function(n, next) {
+    run(cache, n+'', next)
+
+  }, t.end)
+
+})
 
 
+function run (cache, name, cb) {
+  console.time(name)
+  cache(function(err, stream) {
+    stream.on('end', function() {
+      console.timeEnd(name)
+      cb()
+    })
+    .pipe(devnull())
   })
-
-  stream.pipe(ndjson.serialize()).pipe(process.stdout)
-
-  t.ok(true)
-  t.end();
-});
+}
